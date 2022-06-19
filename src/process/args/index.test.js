ff2a203f087b6da13eefd4038d314954e0c02952
Sergey Kukunin <sergey.kukunin@monterosa.co.uk>
2022-06-19 14:16:56 +0200

    update process functions

--- src/process/args/index.test.js
+++ src/process/args/index.test.js
@@ -1,10 +1,1 @@
-import get from './index.js';
-
-test('Process.args', async () => {
-  expect(get({})).toBe('Object');
-  expect(get([])).toBe('Array');
-  expect(get(null)).toBe('Null');
-  expect(get('qwerty')).toBe('String');
-  expect(get(123)).toBe('Number');
-  expect(get()).toBe('Undefined');
-});
+import args from './index.js';
