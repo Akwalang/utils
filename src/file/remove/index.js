ea02f1915939d785739f6b44405a21846be14314
Sergey Kukunin <sergey.kukunin@monterosa.co.uk>
2022-06-19 15:58:42 +0200

    update file functions

--- src/file/remove/index.js
+++ src/file/remove/index.js
@@ -1,6 +1,6 @@
 import fs from 'fs/promises';
 
-import resolve from '../../path/resolve';
+import resolve from '../../path/resolve/index.js';
 
 export default function remove(...path) {
   path = resolve(path);
