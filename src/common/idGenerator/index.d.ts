553e5fb7247dd2ae8ccdd39a41ac008d90904c1f
Sergey Kukunin <sergey.kukunin@monterosa.co.uk>
2022-06-22 02:25:55 +0200

    update common functions types

--- src/common/idGenerator/index.d.ts
+++ src/common/idGenerator/index.d.ts
@@ -1,3 +1,6 @@
-declare function idGenerator(values: any): any;
+type GetId<T> = () => T;
+
+declare function idGenerator(prefix?: null, postfix?: null): GetId<number>;
+declare function idGenerator(prefix?: string, postfix?: string): GetId<string>;
 
 export default idGenerator;
