213a37a399a204a3b010ba4373a9ef130a5b7fef
Sergey Kukunin <sergey.kukunin@monterosa.co.uk>
2022-06-23 02:24:28 +0200

    update exports

--- src/common/index.js
+++ src/common/index.js
@@ -1,5 +1,5 @@
 export { default as deferred } from './deferred/index.js';
 export { default as idGenerator } from './idGenerator/index.js';
-export { default as parse } from './parse/index.js';
+export { default as jsonParse } from './jsonParse/index.js';
 export { default as setAsyncTimeout } from './setAsyncTimeout/index.js';
 export { default as tokenGenerator } from './tokenGenerator/index.js';
--- src/process/index.js
+++ src/process/index.js
@@ -1,1 +1,2 @@
 export { default as args } from './args/index.js';
+export { default as getStack } from './getStack/index.js';
