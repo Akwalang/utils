cb1332cd05d318278d042cd937d3c88bca20fef5
Sergey Kukunin <sergey.kukunin@monterosa.co.uk>
2022-06-25 03:52:45 +0200

    add string functions

---
+++ src/string/replaceAll/index.d.ts
@@ -0,0 +1,3 @@
+declare function replaceAll(value: string, sub: string, replacer: string): string;
+
+export default replaceAll;
---
+++ src/string/replaceAll/index.js
@@ -0,0 +1,3 @@
+export default function replaceAll(value, sub, replacer) {
+  return value.split(sub).join(replacer);
+}
