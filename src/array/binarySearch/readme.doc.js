c063269c965e49d4981e0fa226cb4553bf13624a
Sergey Kukunin <sergey.kukunin@monterosa.co.uk>
2022-06-26 01:48:28 +0200

    update array docs

---
+++ src/array/binarySearch/readme.doc.js
@@ -0,0 +1,95 @@
+export default (pkg, meta, types) => ({
+  title: 'array.binarySearch',
+  sections: [
+    {
+      type: types.SECTION_PARAMS,
+      title: 'binarySearch(array, value[, side = 0])',
+      content: [
+        {
+          type: types.CONTENT_PARAMS,
+          items: [
+            {
+              name: 'array',
+              type: 'T[]',
+              description: 'asc sorted array',
+              defaultValue: null,
+            },
+            {
+              name: 'value',
+              type: 'any | <T>(item: T) => boolean',
+              description: 'searching value or function returns number from -Infinity to Infinity',
+              defaultValue: null,
+            },
+            {
+              name: 'side',
+              type: '-1 | 0 | 1',
+              description: 'search side: _-1_ - left, _0_ - first found, _1_ - right',
+              defaultValue: '0',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      type: types.SECTION_IMPORT,
+      title: 'Import',
+      content: [
+        {
+          type: types.CONTENT_SCRIPT,
+          script: {
+            type: types.SCRIPT_JS,
+            content: `import binarySearch from '${meta.npm.name}/array/binarySearch';`,
+          },
+        },
+      ],
+    },
+    {
+      type: types.SECTION_EXAMPLE,
+      title: 'Example',
+      content: [
+        {
+          type: types.CONTENT_SCRIPT,
+          script: {
+            type: types.SCRIPT_JS,
+            content: `
+              const array = [
+                { value: 1 },
+                { value: 1 },
+                { value: 2 },
+                { value: 2 },
+                { value: 2 },
+                { value: 2 },
+                { value: 3 },
+                { value: 3 },
+                { value: 3 },
+                { value: 3 },
+              ];
+
+              binarySearch(array, item => {
+                if (item.value < 3) return  1;
+                if (item.value > 3) return -1;
+
+                return 0;
+              }, -1);
+            `,
+          },
+        },
+      ],
+    },
+    {
+      type: types.SECTION_RESULT,
+      title: 'Result',
+      content: [
+        {
+          type: types.CONTENT_SCRIPT,
+          script: {
+            type: types.SCRIPT_JSON,
+            content: `
+              6
+            `,
+          },
+        },
+      ],
+    },
+  ],
+});
---
+++ src/array/binarySearch/readme.md
@@ -0,0 +1,43 @@
+## array.binarySearch
+
+#### binarySearch(array, value _[, side = 0]_)
+
+* **array**: T[] - asc sorted array
+* **value**: any | <T>(item: T) => boolean - searching value or function returns number from -Infinity to Infinity
+* **side**: -1 | 0 | 1 _[default: 0]_ - search side: _-1_ - left, _0_ - first found, _1_ - right
+
+#### Import
+
+```javascript
+import binarySearch from '@utils/array/binarySearch';
+```
+
+#### Example
+
+```javascript
+const array = [
+  { value: 1 },
+  { value: 1 },
+  { value: 2 },
+  { value: 2 },
+  { value: 2 },
+  { value: 2 },
+  { value: 3 },
+  { value: 3 },
+  { value: 3 },
+  { value: 3 },
+];
+
+binarySearch(array, item => {
+  if (item.value < 3) return  1;
+  if (item.value > 3) return -1;
+
+  return 0;
+}, -1);
+```
+
+#### Result
+
+```json
+6
+```
